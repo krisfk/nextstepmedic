8ced64cc2d96dcf4ca77c061be2e936870d05166
kaiyu li <krisfk@gmail.com>
2021-09-05 05:02:10 +0800

    .

--- wp-content/themes/nextstepmedic/page-home.php
+++ wp-content/themes/nextstepmedic/page-home.php
@@ -120,11 +120,26 @@ get_header();
         </div>
 
     </div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+
+    <div class="row">
+        <div class="col-6">
+            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x-50.jpg" alt="">
+        </div>
+        <div class="col-6">
+
+            <div class="service-title">Orthopaedics & Traumatology</div>
+
+            <div class="mt-4">Focus on injuries and diseases of the musculoskeletal system (the body’s muscles, skeleton
+                and related
+                connective tissues), including the spine, joints, ligaments, tendons and nerves. To provides
+                musculoskeletal diagnostic services with surgical or non-surgical treatments that are tailored to the
+                individual needs of our patients.</div>
+
+            <a href="" class="brown-round-btn mt-5 d-inline-block">Read More</a>
+        </div>
+
+    </div>
+
 </div>
 
 <script type="text/javascript">
