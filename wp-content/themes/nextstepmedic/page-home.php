31d1748ed238d5be0f86fc9b1592371eb99df6cc
kaiyu li <krisfk@gmail.com>
2021-09-05 04:28:27 +0800

    .

--- wp-content/themes/nextstepmedic/page-home.php
+++ wp-content/themes/nextstepmedic/page-home.php
@@ -13,22 +13,43 @@ get_header();
 
 ?>
 <div class="slides">
-    <div class="position-relative">
-
+    <div class="position-relative slide">
         <div class="row position-absolute w-100 h-100 gx-0 ">
             <div class="col-6"></div>
             <div class="col-6 slide-content-txt-div-wrap">
-
-
                 <div class="row align-items-center h-100">
 
                     <div class="col-12">
                         <div class="slide-content-txt-div">
-
                             The Next Step Medical Clinic is your one-stop orthopaedics,
                             physiotherapy and podiatry centre in Central, Hong Kong for treating musculoskeletal pain,
                             sports
                             injury, foot pain and other related conditions.
+                            <div class="text-center mt-5">
+
+                                <a href="#" class="slide-btns">Learn More</a>
+                                <a href="#" class="slide-btns">Book an Appointment</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/home-banner-1-scaled.jpg" alt="">
+    </div>
+
+    <div class="position-relative slide">
+        <div class="row position-absolute w-100 h-100 gx-0 ">
+            <div class="col-6"></div>
+            <div class="col-6 slide-content-txt-div-wrap">
+                <div class="row align-items-center h-100">
+
+                    <div class="col-12">
+                        <div class="slide-content-txt-div">
+                            Our team of experienced and highly trained medical specialists includes orthopaedics,
+                            physiotherapists and podiatrists work together to provide the best treatment plan for your
+                            individual problems. We proactively treat the causes of the problems, not only the symptoms.
 
 
                             <div class="text-center mt-5">
@@ -40,14 +61,37 @@ get_header();
 
                     </div>
                 </div>
-
-
             </div>
         </div>
-
-        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/home-banner-1-scaled.jpg" alt="">
-
+        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/home-banner-2-scaled.jpg" alt="">
     </div>
+
+    <div class="position-relative slide">
+        <div class="row position-absolute w-100 h-100 gx-0 ">
+            <div class="col-6"></div>
+            <div class="col-6 slide-content-txt-div-wrap">
+                <div class="row align-items-center h-100">
+
+                    <div class="col-12">
+                        <div class="slide-content-txt-div">
+                            To help your recovery from surgery, your weekend sporting injuries and your everyday pain,
+                            get in touch with us.
+
+                            <div class="text-center mt-5">
+
+                                <a href="#" class="slide-btns">Learn More</a>
+                                <a href="#" class="slide-btns">Book an Appointment</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/home-banner-3-scaled.jpg" alt="">
+    </div>
+
+
     <!-- <div class="position-relative">
         <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/home-banner-2-scaled.jpg" alt="">
     </div>
