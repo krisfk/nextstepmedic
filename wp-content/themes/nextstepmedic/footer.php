e86b622c450edd443d816328bf0c7d67c3f37a11
kaiyu li <krisfk@gmail.com>
2021-09-05 17:08:41 +0800

    .

--- wp-content/themes/nextstepmedic/footer.php
+++ wp-content/themes/nextstepmedic/footer.php
@@ -18,7 +18,69 @@
 
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-    fdsf
+
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-4">
+                <ul>
+
+                    <li>Our Services
+                    <li>
+                    <li>Orthopaedics & Traumatology
+                    <li>
+                    <li>Physiotherapy
+                    <li>
+                    <li>Podiatry
+                    <li>
+                    <li>Swift Laser - Wart Treatment
+                    <li>
+                    <li>Lunula Laser - Fungal Nails
+                    <li>
+
+                </ul>
+            </div>
+            <div class="col-4">
+
+                <table>
+                    <tr>
+                        <td>
+                            <!-- Generator: Adobe Illustrator 25.0.0, SVG Export Plug-In  -->
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20.36px"
+                                height="31.67px" viewBox="0 0 20.36 31.67"
+                                style="overflow:visible;enable-background:new 0 0 20.36 31.67;" xml:space="preserve">
+                                <style type="text/css">
+                                .st0 {
+                                    fill: #FFFFFF;
+                                }
+
+                                .st1 {
+                                    fill: #BF9D81;
+                                }
+                                </style>
+                                <defs>
+                                </defs>
+                                <g>
+                                    <path class="st0" d="M10.18,0C6.43,0,0,1.71,0,11.03c0,10.65,10.18,20.64,10.18,20.64s10.18-9.99,10.18-20.64
+		C20.36,2.43,13.93,0,10.18,0z" />
+
+                                    <ellipse transform="matrix(0.0128 -0.9999 0.9999 0.0128 -0.1275 20.2269)"
+                                        class="st1" cx="10.18" cy="10.18" rx="4.59" ry="4.59" />
+                                </g>
+                            </svg>
+                        </td>
+                        <td></td>
+                    </tr>
+
+                </table>
+
+            </div>
+            <div class="col-4"></div>
+        </div>
+    </div>
 
 
 
