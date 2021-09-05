d8a3e7b73ada196b171473e73eab05eb178d8661
kaiyu li <krisfk@gmail.com>
2021-09-05 17:00:42 +0800

    .

--- wp-content/themes/nextstepmedic/page-home.php
+++ wp-content/themes/nextstepmedic/page-home.php
@@ -259,12 +259,12 @@ get_header();
 
 </div>
 
-<div class="book-an-appointment-div pt-4">
+<div class="book-an-appointment-div pt-4 pb-4">
     <a href="#" class="book-an-appointment-a-btn">
         Book an Appointment
     </a>
 
-    <div class="container white font-weight-bold font-italic mt-4 mb-4">
+    <div class="container white font-weight-bold font-italic mt-3">
         Take your NEXT STEP to consult our team and treat your problems proactively.
     </div>
 </div>
