d3c2cac3fb2441eab86e3654b2acf8134af7b109
kaiyu li <krisfk@gmail.com>
2021-09-08 02:41:45 +0800

    .

--- wp-content/themes/nextstepmedic/page-services.php
+++ wp-content/themes/nextstepmedic/page-services.php
@@ -49,21 +49,49 @@ get_header();
 
         <div class="col-4">
             <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-4@2x-50-1.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Orthopaedics & Traumatology</div>
+                <a href="">Read More</a>
+            </div>
         </div>
         <div class="col-4"><img class="w-100"
                 src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-5@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Physiotherapy</div>
+                <a href="">Read More</a>
+            </div>
         </div>
         <div class="col-4"><img class="w-100"
                 src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-6@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Podiatry </div>
+                <a href="">Read More</a>
+            </div>
         </div>
 
     </div>
     <div class="row justify-content-center">
         <div class="col-4"><img class="w-100"
                 src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-7@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">
+
+                    Swift Microwave Technology – <br>
+                    For Wart Treatment
+
+                </div>
+                <a href="">Read More</a>
+            </div>
         </div>
-        <div class="col-4"><img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-8@2x-50.jpg
-        " alt=""></div>
+        <div class="col-4">
+            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-8@2x-50.jpg
+        " alt="">
+            <div class="service-title-div">
+                <div class="service-title">Lunula Laser - For Fungal Nails
+                </div>
+                <a href="">Read More</a>
+            </div>
+        </div>
 
     </div>
 
