12895dc274f88a1db3c73401808c5806ead2b46a
kaiyu li <krisfk@gmail.com>
2021-09-09 04:29:32 +0800

    .

--- wp-content/themes/nextstepmedic/header.php
+++ wp-content/themes/nextstepmedic/header.php
@@ -29,6 +29,8 @@
     </script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
