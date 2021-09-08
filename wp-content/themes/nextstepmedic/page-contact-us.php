7d0c411d5c0f7a578141f1c94c14896dfe9a55fc
kaiyu li <krisfk@gmail.com>
2021-09-08 22:28:52 +0800

    .

---
+++ wp-content/themes/nextstepmedic/page-contact-us.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+
+
+<div class="position-relative">
+
+    <div class="row big-banner-central-row gx-0">
+
+        <div class="col-12">
+
+            <div class="content-div">
+
+                <div class="large-subtitle mb-4">Testimonials
+                </div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+        </div>
+    </div>
+
+    <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-9@2x-50-scaled.jpg" alt="">
+</div>
+
+
+<div class="container testimonials-container mt-5 mb-5">
+
+    <div class="slides">
+
+        <div class="slide">
+
+
+            <a href="#">
+                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x.png" alt="">
+                <div class="mt-4">“I have been continually impressed over the past 7 years with the kind care and
+                    attention that I
+                    have
+                    received from my podiatrist, Charlie. My feet feel great when I leave the clinic.”
+                </div>
+
+                <div class="person-name mt-3">Jen Esposito</div>
+            </a>
+        </div>
+        <div class="slide">
+            <a href="#">
+
+                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x.png" alt="">
+
+                <div class="mt-4">"My physio, Jeff, was super-efficient in dealing with my problems and helping me to
+                    obtain optimum
+                    fitness prior to surgery and following surgery."
+                </div>
+                <div class="person-name mt-3">Taz Freeman</div>
+
+            </a>
+
+        </div>
+        <div class="slide">
+            <a href="#">
+
+                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x.png" alt="">
+
+                <div class="mt-4">“As usual, get to the source of the issue and provide good exercises to remedy.”
+                </div>
+                <div class="person-name mt-3">Aaron Volman</div>
+
+            </a>
+
+        </div>
+        <div class="slide">
+            <a href="#">
+
+                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x.png" alt="">
+
+                <div class="mt-4">“As usual, get to the source of the issue and provide good exercises to remedy.”
+                </div>
+                <div class="person-name mt-3">Aaron Volman</div>
+
+            </a>
+
+        </div>
+        <div class="slide">
+            <a href="#">
+
+                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x.png" alt="">
+
+                <div class="mt-4">“As usual, get to the source of the issue and provide good exercises to remedy.”
+                </div>
+                <div class="person-name mt-3">Aaron Volman</div>
+
+            </a>
+
+        </div>
+
+    </div>
+
+    <!-- 
+    <div class="row mt-5">
+
+        <div class="col-4 service-col">
+            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-4@2x-50-1.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Orthopaedics & Traumatology</div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+        <div class="col-4 service-col"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-5@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Physiotherapy</div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+        <div class="col-4 service-col"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-6@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Podiatry </div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+
+    </div>
+    <div class="row justify-content-center mt-5 mb-5">
+        <div class="col-4 service-col"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-7@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">
+
+                    Swift Microwave Technology – <br>
+                    For Wart Treatment
+
+                </div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+        <div class="col-4 service-col">
+            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-8@2x-50.jpg
+        " alt="">
+            <div class="service-title-div">
+                <div class="service-title">Lunula Laser - For Fungal Nails
+                </div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+
+    </div> -->
+
+
+
+
+
+
+
+
+
+</div>
+
+
+<script type="text/javascript">
+$('.slides').slick({
+    dots: false,
+    arrows: true,
+    autoplay: false,
+    pauseOnFocus: false,
+    infinite: true,
+    speed: 800,
+    autoplaySpeed: 5000,
+    cssEase: 'ease-out',
+    pauseOnHover: false,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
+</script>
+
+
+
+
+<?php
+
+
+get_footer();
