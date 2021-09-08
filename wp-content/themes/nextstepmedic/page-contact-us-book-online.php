6446192f7b28217b427bd2cac1167418b187dfb9
kaiyu li <krisfk@gmail.com>
2021-09-08 22:58:45 +0800

    .

--- wp-content/themes/nextstepmedic/page-contact-us-book-online.php
+++ wp-content/themes/nextstepmedic/page-contact-us-book-online.php
@@ -48,7 +48,7 @@ get_header();
 
                     <div class="col-12">
 
-                        <div class="row">
+                        <div class="row gx-0">
 
                             <div class="col-2">
                                 <img class="w-100"
