4350d5932e1c5bcc175db1bf89260e5c73afc24a
kaiyu li <krisfk@gmail.com>
2021-09-08 22:29:28 +0800

    .

--- wp-content/themes/nextstepmedic/page-contact-us-book-online.php
+++ wp-content/themes/nextstepmedic/page-contact-us-book-online.php
@@ -22,19 +22,13 @@ get_header();
 
             <div class="content-div">
 
-                <div class="large-subtitle mb-4">Testimonials
-                </div>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
             </div>
         </div>
     </div>
 
-    <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-9@2x-50-scaled.jpg" alt="">
+    <!-- <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-9@2x-50-scaled.jpg" alt=""> -->
 </div>
 
 
