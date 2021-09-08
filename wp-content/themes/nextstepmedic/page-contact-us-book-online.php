4525fc66fb8f22ea0b91e9a2984e6054d3829394
kaiyu li <krisfk@gmail.com>
2021-09-09 04:19:08 +0800

    .

--- wp-content/themes/nextstepmedic/page-contact-us-book-online.php
+++ wp-content/themes/nextstepmedic/page-contact-us-book-online.php
@@ -90,7 +90,7 @@ get_header();
 
                 <div id="map"></div>
 
-                <?php echo get_template_directory_uri().'/assets/images/pin.svg';?>
+                <?php //echo get_template_directory_uri().'/assets/images/pin.svg';?>
                 <!-- <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x-1.png
 " alt=""> -->
             </div>
@@ -253,7 +253,7 @@ function initMap() {
     var marker2 = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        icon: 'https://www.ehkd.com/wp-content/themes/ehkd/assets/images/ehkd-marker-svg.svg'
+        icon: 'https://nextstepmedic.com/wp-content/themes/nextstepmedic/assets/images/pin.svg'
     });
 
 
