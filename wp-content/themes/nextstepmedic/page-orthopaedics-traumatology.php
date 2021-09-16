0f77f61cfaa1e1d788b3f481f3fabafe2e833e40
kaiyu li <krisfk@gmail.com>
2021-09-16 23:51:47 +0800

    .

--- wp-content/themes/nextstepmedic/page-orthopaedics-traumatology.php
+++ wp-content/themes/nextstepmedic/page-orthopaedics-traumatology.php
@@ -53,38 +53,48 @@ get_header();
             </div>
         </div>
 
-        <div class="col-12 mt-5">
-            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x-50-1-scaled.jpg"
-                alt="">
-            <div class="brown-bg pt-4 pe-5 ps-5 pb-4">
+        <div class="row mt-5">
+            <div class="col-12">
+                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x-50-1-scaled.jpg"
+                    alt="">
+                <div class="brown-bg pt-4 pe-5 ps-5 pb-4">
 
-                <div class="service-subtitle white">Our Orthopaedics Services
-                </div>
-                <div class="mt-3">The Next Step provides musculoskeletal diagnostic services, surgical and non-surgical
-                    treatments that are tailored to the individual needs of our patients. Our orthopaedic specialists
-                    treat patients suffering from a comprehensive range of orthopaedic conditions of the knee, shoulder
-                    and elbow, foot and ankle, hand and wrist, hip and spine.
+                    <div class="service-subtitle white">Our Orthopaedics Services
+                    </div>
+                    <div class="mt-3">The Next Step provides musculoskeletal diagnostic services, surgical and
+                        non-surgical
+                        treatments that are tailored to the individual needs of our patients. Our orthopaedic
+                        specialists
+                        treat patients suffering from a comprehensive range of orthopaedic conditions of the knee,
+                        shoulder
+                        and elbow, foot and ankle, hand and wrist, hip and spine.
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="row mt-5">
 
-        <div class="col-6">
-            <div class="service-subtitle">What is Orthopaedics & Traumatology?</div>
-            <div class="mt-3">Orthopaedics focuses on injuries and diseases of the musculoskeletal system (the body’s
-                muscles,
-                skeleton and related connective tissues), including the spine, joints, ligaments, tendons and nerves.
-                <br> <br>
-                Explore our available private orthopaedics treatment options below and book your appointment today so
-                that you can start to feel good again.
+            <div class="col-6">
+                <div class="service-subtitle">What is Orthopaedics & Traumatology?</div>
+                <div class="mt-3">Orthopaedics focuses on injuries and diseases of the musculoskeletal system (the
+                    body’s
+                    muscles,
+                    skeleton and related connective tissues), including the spine, joints, ligaments, tendons and
+                    nerves.
+                    <br> <br>
+                    Explore our available private orthopaedics treatment options below and book your appointment today
+                    so
+                    that you can start to feel good again.
+                </div>
+                <div class="mt-3">
+                    <a href="#" class="brown-round-btn">Book an Appointment</a>
+                </div>
             </div>
-            <div class="mt-3">
-                <a href="#" class="brown-round-btn">Book an Appointment</a>
-            </div>
-        </div>
 
 
-        <div class="col-6"><img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-1@2x-50.jpg
+            <div class="col-6"><img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-1@2x-50.jpg
         " alt=""></div>
+        </div>
 
     </div>
 
