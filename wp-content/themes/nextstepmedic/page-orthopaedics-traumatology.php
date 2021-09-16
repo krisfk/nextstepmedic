33ddcded5f47a01f033b6c81e969586ffe87eb1d
kaiyu li <krisfk@gmail.com>
2021-09-16 23:07:46 +0800

    .

---
+++ wp-content/themes/nextstepmedic/page-orthopaedics-traumatology.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+
+
+<div class="position-relative">
+
+    <div class="row big-banner-central-row gx-0">
+
+        <div class="col-12">
+
+            <div class="content-div">
+
+                <div class="large-subtitle mb-4">Services
+                </div>
+                The Next Step Medical Clinic is your one-stop orthopaedics, physiotherapy and podiatry centre for
+                treating musculoskeletal pain, sports injury, foot pain and other related conditions. To help your
+                recovery from surgery, your weekend sporting injuries and your everyday pain, get in touch with us.
+                <br> <br>
+                Our team of experienced and highly trained medical specialists includes orthopaedics, physiotherapists
+                and podiatrists work together to provide the best treatment plan for your individual problems. We
+                proactively treat the causes of the problems, not only the symptoms.
+                <br> <br>
+                We strongly believe in delivering greater value and higher quality of care to our patients through
+                effective communication and ethical clinical practice.
+            </div>
+        </div>
+    </div>
+
+    <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-1-scaled.jpg" alt="">
+</div>
+
+
+<div class="container">
+
+
+    <div class="row mt-5">
+
+        <div class="col-4 service-col">
+            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-4@2x-50-1.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Orthopaedics & Traumatology</div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+        <div class="col-4 service-col"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-5@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Physiotherapy</div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+        <div class="col-4 service-col"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-6@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">Podiatry </div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+
+    </div>
+    <div class="row justify-content-center mt-5 mb-5">
+        <div class="col-4 service-col"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-7@2x-50.jpg" alt="">
+            <div class="service-title-div">
+                <div class="service-title">
+
+                    Swift Microwave Technology – <br>
+                    For Wart Treatment
+
+                </div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+        <div class="col-4 service-col">
+            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-8@2x-50.jpg
+        " alt="">
+            <div class="service-title-div">
+                <div class="service-title">Lunula Laser - For Fungal Nails
+                </div>
+                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+<div class="book-an-appointment-div pt-4 pb-4">
+    <a href="#" class="book-an-appointment-a-btn">
+        Book an Appointment
+    </a>
+
+    <div class="container white fw-bolder fst-italic mt-3">
+        Take your NEXT STEP to consult our team and treat your problems proactively.
+    </div>
+</div>
+
+
+
+
+
+<?php
+
+
+get_footer();
