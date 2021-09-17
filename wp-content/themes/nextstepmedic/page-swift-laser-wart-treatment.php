8d61c48994ea185fb2d27c32174a05034f203a94
kaiyu li <krisfk@gmail.com>
2021-09-18 00:09:39 +0800

    .

--- wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
+++ wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
@@ -98,61 +98,25 @@ get_header();
                     <li>A lesion that interrupts the normal lines and ridges in the skin of your foot</li>
                     <li>Pain or tenderness when walking or standing</li>
                 </ul>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             </div>
         </li>
         <li><a href="javascript:void(0);">What are the causes of warts?
             </a>
             <div class="accordion-content">
-                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-8@2x-50-1-scaled.jpg"
-                    alt="">
-                <div class="mt-3">Muscles generally work together in pairs to help promote good quality pain free
-                    movement at a
-                    joint. If you overuse certain muscles due to repetitive strain, sports, prolonged postures or
-                    weakness in other parts of your body, you can develop tightness and dominance within one of the
-                    pair. If the situation continues, stress and altered loading to the soft tissues may cause you
-                    to
-                    develop pain, tendinopathies, ligament sprains, muscle spasms, and the list is endless.
-                    <br><br>
-                    A qualified physiotherapist will help assess you for any muscle imbalances to include tightness
-                    and
-                    weakness of the soft tissues. Effective methods of treating such problems will be outlined in
-                    forms
-                    of things you can do to help yourself and techniques we can perform within our treatment to
-                    help.
-                    <br><br>
-                    Stretching is a technique used to increase the length of muscles and tendons in order to improve
-                    the
-                    effectiveness of the muscle fibres, improve joint range of motion and relieve pain. Your
-                    Physiotherapist will advise appropriate stretches to correct muscle length and elasticity.
-                    Muscle
-                    energy techniques or Proprioceptive Neuromuscular Facilitation stretches are used to lengthen
-                    muscles through a hold and relax muscle contraction technique, this generally requires the
-                    assistance of your therapist and is widely used by athletes to improve range of movement in a
-                    joint
-                    both before and after sport to improve performance and reduce risk of injury.
+
+                <div class="row">
+                    <div class="col-6"><img class="w-100"
+                            src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-12@2x-50-1.jpg" alt="">
+                    </div>
+                    <div class="col-6">
+                        Plantar warts are caused by an infection with HPV in the outer layer of skin on the soles of
+                        your feet. They develop when the virus enters your body through tiny cuts, breaks or other weak
+                        spots on the bottoms of your feet. HPV is very common, and more than 100 kinds of the virus
+                        exist. But only a few of them cause warts on the feet. Other types of HPV are more likely to
+                        cause warts on other areas of your skin or on mucous membranes.
 
 
-
-
-
-
-
-
+                    </div>
 
                 </div>
             </div>
