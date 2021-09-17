96b7bc3a96703a03e258a40805eac048a95b2bab
kaiyu li <krisfk@gmail.com>
2021-09-17 17:29:59 +0800

    .

--- wp-content/themes/nextstepmedic/page-podiatry.php
+++ wp-content/themes/nextstepmedic/page-podiatry.php
@@ -126,7 +126,7 @@ get_header();
         <div class="col-6"><img class="w-100"
                 src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-4.jpg" alt=""></div>
         <div class="col-6">
-            <div class="">General Podiatry Care
+            <div class="grey-bold">General Podiatry Care
 
             </div>
             <div class="mt-3">
