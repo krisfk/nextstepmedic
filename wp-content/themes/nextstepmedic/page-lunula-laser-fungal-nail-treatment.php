e3a9a7ca594170795424047f51981a64974cd90f
kaiyu li <krisfk@gmail.com>
2021-09-18 00:22:46 +0800

    .

---
+++ wp-content/themes/nextstepmedic/page-lunula-laser-fungal-nail-treatment.php
@@ -0,0 +1,488 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+
+
+<div class="position-relative">
+
+    <div class="row big-banner-central-row position-relative gx-0 align-items-center">
+
+        <div class="col-12">
+
+            <div class="content-div service-title-div">
+
+                <h1 class="service-h1-title">Swift Laser Wart Treatment</h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-1-scaled.jpg" alt=""> -->
+</div>
+
+
+<div class="container">
+
+
+    <div class="row mt-5">
+
+
+        <div class="col-6">
+
+
+            <div class="row">
+                <div class="col-6">
+                    <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-5.jpg"
+                        alt="">
+                    <div class="mt-2">Deep plantar wart</div>
+                </div>
+                <div class="col-6"><img class="w-100"
+                        src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-4@2x-50-5.jpg" alt="">
+                    <div class="mt-2">Young plantar warts</div>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="col-6">
+            <div class="service-subtitle">What Are Plantar Warts?</div>
+            <div class="mt-3">Plantar warts or verruca are small growths that usually appear on the heels or other
+                weight-bearing areas of your feet. This pressure may also cause plantar warts to grow inward beneath a
+                hard, thick layer of skin (callus).
+
+            </div>
+            <div class="mt-3">
+                <a href="#" class="brown-round-btn">Book an Appointment</a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+    <div class="service-subtitle mt-5">FAQ</div>
+
+
+
+    <ul class="accordion">
+        <li><a href="javascript:void(0);"> What are the symptoms of plantar warts?
+            </a>
+
+            <div class="accordion-content">
+
+                <ul>
+                    <li>A small, fleshy, rough, grainy growth (lesion) on the bottom of your foot, usually the base of
+                        the
+                        toes and forefoot or the heel</li>
+                    <li>Hard, thickened skin (callus) over a well-defined "spot" on the skin, where a wart has grown
+                        inward</li>
+                    <li>Black pinpoints, which are commonly called wart seeds but are actually small, clotted blood
+                        vessels</li>
+                    <li>A lesion that interrupts the normal lines and ridges in the skin of your foot</li>
+                    <li>Pain or tenderness when walking or standing</li>
+                </ul>
+            </div>
+        </li>
+        <li><a href="javascript:void(0);">What are the causes of warts?
+            </a>
+            <div class="accordion-content">
+
+                <div class="row">
+                    <div class="col-6"><img class="w-100"
+                            src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-12@2x-50-1.jpg" alt="">
+                    </div>
+                    <div class="col-6">
+                        Plantar warts are caused by an infection with HPV in the outer layer of skin on the soles of
+                        your feet. They develop when the virus enters your body through tiny cuts, breaks or other weak
+                        spots on the bottoms of your feet. HPV is very common, and more than 100 kinds of the virus
+                        exist. But only a few of them cause warts on the feet. Other types of HPV are more likely to
+                        cause warts on other areas of your skin or on mucous membranes.
+
+
+                    </div>
+
+                </div>
+            </div>
+
+        </li>
+        <li><a href="javascript:void(0);"> Is the virus transmissible?
+            </a>
+            <div class="accordion-content">
+
+
+                <div class="row">
+                    <div class="col-6">
+                        The HPV strains that cause plantar warts are not highly contagious. The virus is not easily
+                        transmitted by direct contact from one person to another. But it thrives in warm, moist
+                        environments. Consequently, you may contract the virus by walking barefoot around swimming pools
+                        or locker rooms. If the virus spreads from the first site of infection, more warts may appear.
+
+
+                    </div>
+                    <div class="col-6"><img class="w-100"
+                            src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-13@2x-50-1.jpg" alt="">
+                    </div>
+
+
+                </div>
+
+
+
+
+            </div>
+        </li>
+
+
+
+    </ul>
+
+
+    <div class="service-subtitle mt-5">Treatments for Plantar Warts and Palmar Warts</div>
+
+    <div class="row mt-5">
+
+
+        <div class="col-6"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-5@2x-50-4.jpg" alt=""></div>
+
+
+        <div class="col-6">
+            <div class="">
+                In some cases, a plantar wart will resolve naturally. Other cases may require
+                treatment. The Next Step Medical Clinic provide you alternative treatment options:
+
+            </div>
+
+            <ul class="ps-3 mt-3">
+                <li>Curettage, which involves cutting out the wart</li>
+                <li>Liquid nitrogen, which can be used to “freeze” off the wart</li>
+                <li>Laser therapy</li>
+                <li>Natural remedies</li>
+                <li>Salicylic acid cream, a topical cream that can “burn” off the wart</li>
+                <li>Topical medication applied directly to the wart</li>
+            </ul>
+
+        </div>
+
+
+    </div>
+
+    <div class="mt-5">
+
+        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-12@2x-50-3-scaled.jpg"
+            alt="">
+    </div>
+
+
+    <div class="service-subtitle mt-5">What is SWIFT?</div>
+
+    <div class="mt-5">SWIFT is a cutting-edge, FDA cleared technology that has proven to be highly effective in the
+        removal of
+        plantar warts. It delivers low dose microwave energy through a specialized probe that targets and treats the
+        underlying HPV virus by stimulating a natural immune response in the body. SWIFT technology addresses the root
+        cause and not the symptoms of plantar warts, thus preventing its recurrence.
+    </div>
+
+
+    <div class="service-subtitle mt-5">FAQ</div>
+
+
+
+    <ul class="accordion">
+        <li><a href="javascript:void(0);">How does SWIFT Wart Therapy work?
+
+            </a>
+
+            <div class="accordion-content">
+
+                <div class="row">
+
+                    <div class="col-6">Wart Therapy delivers a precise, highly controlled energy dose to the infected
+                        tissue. As microwaves travel into the tissue, water molecules begin colliding and creating
+                        localized heat energy. Within seconds, the treatment is complete and the immune response begins.
+                        Heat shock proteins within HPV infected cells are released, alerting the body to the presence of
+                        the virus.
+                    </div>
+                    <div class="col-6">
+                        <img class="w-100"
+                            src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-14@2x-50-1.jpg" alt="">
+                    </div>
+                </div>
+
+
+
+
+            </div>
+        </li>
+        <li><a href="javascript:void(0);">How is SWIFT a better wart therapy than other treatments?
+
+            </a>
+            <div class="accordion-content">
+
+                <div class="mt-3">
+                    <ul class="grey-bold">
+                        <li>Faster</li>
+                    </ul>
+                    Each treatment lasts only 5-10 minutes, consisting of 3-5 very short application of the SWIFT device
+                    (usually 2 seconds in duration). SWIFT delivers a precise, highly controlled dose of microwave
+                    energy to a pre-determined depth into the skin.
+
+                    <ul class="grey-bold mt-4">
+                        <li>Cleaner</li>
+                    </ul>
+                    Microwave therapy does not produce smoke, plume, or smell that laser or electrocautery devices
+                    produce. The SWIFT device has been designed to ensure microwaves travel in straight lines with no
+                    lateral spread, commonly seen in Cryotherapy. This results in a treatment that can be used to
+                    accurately target
+                    tissue and leave healthy tissue undamaged.
+                    <br><br>
+                    The SWIFT device also features a disposable tip – which removes the need for sterilization between
+                    patients and eliminates the risk of cross-contamination.
+                    <br><br>
+
+                    The precision treatment of SWIFT does not break the skin – meaning no blisters, no padding, no
+                    scarring – overall resulting to a cleaner, safer alternative.
+
+                    <ul class="grey-bold  mt-4">
+                        <li>Digital Time and Power dials: ensure consistent and precise energy delivery</li>
+                        <li>Advanced alarm systems: ensure maximum safety for patients</li>
+                        <li>Internal components: similar to those used in interventional oncology</li>
+                    </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+
+        </li>
+        <li><a href="javascript:void(0);">Does SWIFT Wart Treatment hurt?
+
+            </a>
+            <div class="accordion-content">
+
+
+                As the treatment involves a sudden increase in temperature, It is likely you will feel some discomfort,
+                like a sharp sensation, similar to an injection. However, each dose of energy administered only last 2
+                seconds, so your discomfort will quickly subside. 
+
+
+
+
+
+            </div>
+        </li>
+
+
+        <li><a href="javascript:void(0);">How will I feel after SWIFT Wart Treatment?
+
+
+            </a>
+            <div class="accordion-content">
+
+                In some cases, the treated area may feel sore but will not affect you undertaking normal daily
+                activities. 
+
+
+
+            </div>
+        </li>
+        <li><a href="javascript:void(0);">How many treatments will I need?
+
+
+
+            </a>
+            <div class="accordion-content">
+                SWIFT protocol involves between 3 and 4 treatments, spaced 4 weeks apart; aligning with the body’s
+                natural immune cycle. Each treatment lasts 15 minutes.
+
+            </div>
+
+
+        </li>
+    </ul>
+
+
+    <div class="service-subtitle mt-5">Feedback From Practitioners
+    </div>
+
+    <div class="row mt-2">
+
+        <div class="col-6"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-7@2x-50-3.jpg" alt="">
+        </div>
+        <div class="col-6"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-8@2x-50-3.jpg" alt="">
+        </div>
+    </div>
+
+    <div class="service-subtitle mt-5">Patient Reviews
+
+    </div>
+
+    <div class="row mt-2">
+
+        <div class="col-6"><img class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-9@2x-50-3.jpg" alt="">
+        </div>
+    </div>
+
+    <div class="service-subtitle mt-5">For More Details</div>
+
+    <div class="mt-2">
+        <div class="row g-0">
+            <div class="col-3"><img class="leaflet-img"
+                    src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-10@2x-50-2.jpg" alt="">
+
+                <div class="mt-3"> <a href="javascript:void(0);" class="find-out-more-btn brown-round-btn ">Find Out
+                        More</a>
+                </div>
+            </div>
+            <div class="col-3"><img class="leaflet-img"
+                    src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-11@2x-50-1.jpg" alt="">
+
+                <div class="mt-3"> <a href="javascript:void(0);" class="find-out-more-btn brown-round-btn ">Find Out
+                        More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="sep mt-4"></div>
+
+    <div class="row mt-4">
+        <div class="col-6"><img style="height:400px" class="w-100"
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x.png" alt="">
+        </div>
+        <div class="col-6">
+
+            <div class="gold">
+                <div class="person-name">
+                    Charlie S.K Lau
+                </div>
+
+                <div class="mt-3">
+                    Registered Podiatrist (NZ) <br>
+                    BHsc Podiatry (NZ)<br>
+                    PgD in Musculoskeletal Medicine and Rehabilitation (CUHK)<br>
+                    Member of PodiatryNZ<br>
+                    Member of International Podiatrists Association of Hong Kong (IPAHK)
+                </div>
+            </div>
+
+            <div class="mt-3">Charlie is a podiatrist registered under the International Podiatrists Association of Hong
+                Kong and
+                registered with the Podiatrists Board of New Zealand. He received professional training in podiatry from
+                the Auckland university technology in New Zealand and furthered his education and obtained his
+                Postgraduate Diploma in Musculoskeletal Medicine and Rehabilitation from the Chinese University of Hong
+                Kong.
+                <br><br>
+                Charlie is currently working at the Matilda international hospital/ Matilda Medical Centre (Central) ,
+                Hong Kong Adventist Hospital- Tsuen wan and Gleneagles Hospital Hong Kong. He had worked in two
+                multidisciplinary clinics in Shanghai. He also volunteered at the HKSKH Lady MacLehose Centre and
+                provided podiatry education for the disadvantaged.
+                Charlie work closely in partnership with consultant orthopedic surgeons, general practitioner and
+                physiotherapist. They ensure each patient is thoroughly assessed; diagnosed and optimum treatment is
+                selected.
+                <br><br>
+                His experience in a private practice in New Zealand and at the Matilda International Hospital in Hong
+                Kong has provided him with a comprehensive understanding of diagnosis and treatment of a wide array of
+                pathologies of the foot and lower leg, including:
+
+                <ul class=" ps-3">
+                    <li>Athletic and sports injuries of the foot, knee, and hip</li>
+                    <li>Biomechanical assessments (examination of the lower limbs, looking at their structure,
+                        alignment,
+                        strengths and weaknesses)</li>
+                    <li>Chronic pathologies</li>
+                    <li>Diabetic foot care</li>
+                    <li>Footwear assessment</li>
+                    <li>Nail & skin pathologies (callus / cracked heels, corns, in-growing toenails, fungal nails,
+                        verrucae, foot warts, etc.)</li>
+                    <li>Neurological disorders</li>
+                    <li>Orthotic prescription</li>
+                    <li>Paediatric assessment</li>
+                    <li>Rheumatoid arthritis</li>
+                    <li>Vascular & musculoskeletal disorders</li>
+
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<div class="mt-5">
+    <?php echo get_template_part( 'book' );?>
+
+</div>
+
+
+<script type="text/javascript">
+$(function() {
+
+    $('.accordion a').click(function() {
+        $('.accordion a').removeClass('active');
+        $(this).addClass('active');
+        $('.accordion-content').fadeOut(0);
+        $(this).next('.accordion-content').slideDown(200);
+
+        // $('body,html').
+        $("body,html").scrollTop($(this).offset().top);
+
+
+    })
+})
+</script>
+
+<?php
+
+
+get_footer();
