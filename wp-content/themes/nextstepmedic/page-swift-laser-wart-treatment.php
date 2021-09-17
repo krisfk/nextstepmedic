db14618dc383a82e3ddef8315bbbd38716447a6e
kaiyu li <krisfk@gmail.com>
2021-09-17 23:34:00 +0800

    .

--- wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
+++ wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
@@ -79,8 +79,11 @@ get_header();
 
     <div class="service-subtitle">FAQ</div>
 
+
+
     <ul class="accordion">
-        <li><a href="javascript:void(0);">Mobilisations</a>
+        <li><a href="javascript:void(0);"> What are the symptoms of plantar warts?
+            </a>
 
             <div class="accordion-content">
 
@@ -114,7 +117,8 @@ get_header();
 
             </div>
         </li>
-        <li><a href="javascript:void(0);">Stretches</a>
+        <li><a href="javascript:void(0);">What are the causes of warts?
+            </a>
             <div class="accordion-content">
                 <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-8@2x-50-1-scaled.jpg"
                     alt="">
@@ -156,7 +160,8 @@ get_header();
             </div>
 
         </li>
-        <li><a href="javascript:void(0);">Manipulations</a>
+        <li><a href="javascript:void(0);"> Is the virus transmissible?
+            </a>
             <div class="accordion-content">
 
                 <div class="row">
@@ -185,184 +190,8 @@ get_header();
 
             </div>
         </li>
-        <li><a href="javascript:void(0);">Ultrasound</a>
-            <div class="accordion-content">
-
-                <div class="row">
-
-                    <div class="col-6">
-                        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-10@2x-50.jpg"
-                            alt="">
-                    </div>
-                    <div class="col-6"> Ultrasound is a small electrical device used to send high frequency sound
-                        waves into your problem tissues. The sound waves penetrate the tissue and stimulate the
-                        cells involved in healing to reduce inflammation and promote tissue repair. Ultrasound can
-                        be applied at any stage of healing, your therapist will alter the dose according to your
-                        injury and phase of healing.
-                        <br> <br>
-                        Ultrasound treatment helps you to recover after acute injury, and may speed up recovery
-                        times. Ultrasound at a high dose is also helpful in the management of calcific
-                        tendinopathies to help break down and aid reabsorption of the calcium ions within the
-                        tendon.
 
 
-
-
-
-
-
-
-
-
-                    </div>
-
-                </div>
-
-
-
-
-            </div>
-        </li>
-        <li><a href="javascript:void(0);">Taping</a>
-
-            <div class="accordion-content">
-                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-11@2x-50-scaled.jpg"
-                    alt="">
-                <div class="mt-3">Physiotherapists can use taping in order to relieve pain, improve joint stability,
-                    offload
-                    joints, prevent injury, reduce injury reoccurrence, improve muscle function and restore normal
-                    movement.
-                    <br><br>
-                    We use a variety of tapes from zinc oxide for joint stability, to Elastic Adhesive Bandage (EAB)
-                    to compress swelling. Over recent years, Kinesiology tape (K-tape) has become very popular. This
-                    comes in a variety of colours and is unique in the fact it allows full multidirectional
-                    movement- but keeps the brain vital feedback on where that particular body part is in relation
-                    to the desired movement and movement pattern.
-
-
-
-
-
-
-
-
-
-
-                </div>
-            </div>
-        </li>
-        <li><a href="javascript:void(0);">Massage</a>
-            <div class="accordion-content">
-
-                <div class="row">
-
-                    <div class="col-6">
-                        Massage is a soft tissue technique with direct influence on the muscles, tendons, ligaments
-                        and other connective tissue. Massage has a number of physiological benefits including
-                        increasing; circulation and oxygen flow, reducing inflammation and releasing endorphins.
-                        Massage aims to alleviate aches and pains by relaxing muscles, improving joint range and
-                        flexibility and ultimately speeding up recovery time.
-                    </div>
-
-                    <div class="col-6">
-                        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-12@2x-50.jpg"
-                            alt="">
-                    </div>
-
-
-                </div>
-
-
-
-
-            </div>
-        </li>
-        <li><a href="javascript:void(0);">TENS</a>
-            <div class="accordion-content">
-
-                <div class="row">
-
-
-                    <div class="col-6">
-                        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-13@2x-50.jpg"
-                            alt="">
-                    </div>
-
-                    <div class="col-6">
-
-                        TENS is a method of electrical stimulation which primarily aims to provide a degree of
-                        symptomatic pain relief by exciting sensory nerves and thereby stimulating either the pain
-                        gate
-                        mechanism and/or the opioid system. It is used to relieve acute and chronic pain including:
-                        <ul class="mt-3">
-                            <li>Acute pain – such as sports injuries, acute torticollis, acute lower back pain, and
-                                joint
-                                pain.</li>
-                            <li>Chronic pain – such as low back pain, rheumatoid arthritis, peripheral nerve
-                                injuries and
-                                phantom limb pain.</li>
-                        </ul>
-
-                    </div>
-
-
-
-                </div>
-            </div>
-        </li>
-        <li><a href="javascript:void(0);">Myofascial Release</a>
-            <div class="accordion-content">
-                <div class="row">
-
-
-                    <div class="col-6">
-                        <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-14@2x-50.jpg"
-                            alt="">
-                    </div>
-
-                    <div class="col-6">
-
-
-                        Myofascial release is a specialised physical and manual therapy used in the treatment of
-                        this
-                        form of superficial connective soft tissue. The treatment method involves the application of
-                        soft, yet firm, continuous pressure into the area of tension. The treatment aims to reduce
-                        discomfort and restore normal tissue movement by causing an increased blood flow and the
-                        fascia
-                        to elongate. <br> <br>
-
-                        Myofascial release is a fantastic tool to help manage conditions caused by restrictions
-                        within
-                        tight structures within the kinetic chain, myofascial release is helpful in the management
-                        of
-                        many musculoskeletal conditions, to name a few, we may use this type of technique to help
-                        our
-                        patients that suffer with plantar fasciitis, greater trochanteric pain syndromes, snapping
-                        hip,
-                        carpal tunnel syndromes, and low back pain.
-
-
-
-                    </div>
-
-
-                </div>
-            </div>
-        </li>
-        <li><a href="javascript:void(0);">Home Exercises</a>
-            <div class="accordion-content">
-                <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-15@2x-50-scaled.jpg"
-                    alt="">
-                <div class="mt-3">Your physiotherapist will fully assess your problem area to help you discover
-                    exercises to
-                    address the cause of your symptoms or to compliment manual therapy administered. Research shows
-                    that home exercises targeted to strengthen, stretch, load tissue or improve motor control are
-                    essential in your recovery and management of long-term conditions. When combined with
-                    therapeutic intervention, progressed accordingly, in line with your personal goals and current
-                    regime you will experience less pain, increased function and potentially improved performance.
-                </div>
-            </div>
-        </li>
 
     </ul>
 
