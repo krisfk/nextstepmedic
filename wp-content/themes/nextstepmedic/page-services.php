d5cf1c2c4880cbed41bb7104b37fa3c6223fd229
kaiyu li <krisfk@gmail.com>
2021-09-18 18:29:00 +0800

    .

--- wp-content/themes/nextstepmedic/page-services.php
+++ wp-content/themes/nextstepmedic/page-services.php
@@ -51,7 +51,7 @@ get_header();
             <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-4@2x-50-1.jpg" alt="">
             <div class="service-title-div">
                 <div class="service-title">Orthopaedics & Traumatology</div>
-                <a href="javascript:void(0);" class="read-more-btn">Read More</a>
+                <a href="<?php get_site_url()?>/orthopaedics-traumatology" class="read-more-btn">Read More</a>
             </div>
         </div>
         <div class="col-4 service-col"><img class="w-100"
