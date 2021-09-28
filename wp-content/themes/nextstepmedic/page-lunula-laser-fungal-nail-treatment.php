bb0e6022df492b5e0382c8ae466895597ffc4b5b
kaiyu li <krisfk@gmail.com>
2021-09-28 23:20:04 +0800

    .

--- wp-content/themes/nextstepmedic/page-lunula-laser-fungal-nail-treatment.php
+++ wp-content/themes/nextstepmedic/page-lunula-laser-fungal-nail-treatment.php
@@ -65,8 +65,11 @@ get_header();
 
             </div>
             <div class="mt-3">
-                <a href="<?php echo get_site_url();?>/contact-us-book-online" class="brown-round-btn">Book an
-                    Appointment</a>
+                <!-- <a href="<?php echo get_site_url();?>/contact-us-book-online" class="brown-round-btn">Book an
+                    Appointment</a> -->
+                <a href="<?php echo get_site_url();?>/contact-us-book-online" class="book-an-appointment-a-btn">
+                    Book an Appointment
+                </a>
             </div>
         </div>
     </div>
