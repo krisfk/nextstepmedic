d4a2786a17ec6f7de76b4370b6e2d7d55a9e6e62
kaiyu li <krisfk@gmail.com>
2021-09-30 04:15:35 +0800

    .

--- wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
+++ wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
@@ -468,7 +468,8 @@ get_header();
 ">
 
 
-            <iframe src="https://player.vimeo.com/video/203867791?h=91ec8079e0&title=0&byline=0&portrait=0" width="640"
+            <iframe class="vimeo-iframe"
+                src="https://player.vimeo.com/video/203867791?h=91ec8079e0&title=0&byline=0&portrait=0" width="640"
                 height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
 
 
@@ -479,7 +480,8 @@ get_header();
 ">
 
 
-            <iframe src="https://player.vimeo.com/video/203867791?h=91ec8079e0&title=0&byline=0&portrait=0" width="640"
+            <iframe class="vimeo-iframe"
+                src="https://player.vimeo.com/video/203867791?h=91ec8079e0&title=0&byline=0&portrait=0" width="640"
                 height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
 
 
