30a096181489045ca5b32b65c82acf1a41924ba6
kaiyu li <krisfk@gmail.com>
2021-09-30 04:33:42 +0800

    .

--- wp-content/themes/nextstepmedic/page-lunula-laser-fungal-nail-treatment.php
+++ wp-content/themes/nextstepmedic/page-lunula-laser-fungal-nail-treatment.php
@@ -270,6 +270,10 @@ get_header();
         <div class="col-6 position-relative">
 
 
+            <iframe class="vimeo-iframe" src="https://player.vimeo.com/video/174575646?h=64b2315f11" frameborder="0"
+                allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+
+
 
             <img class="w-100 " src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-24@2x-50.jpg" alt="">
 
