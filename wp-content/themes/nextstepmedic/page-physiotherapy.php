152a6296a66e54efbbd4276ba70ebc74c11c5af4
kaiyu li <krisfk@gmail.com>
2021-09-30 02:59:02 +0800

    .

--- wp-content/themes/nextstepmedic/page-physiotherapy.php
+++ wp-content/themes/nextstepmedic/page-physiotherapy.php
@@ -164,6 +164,27 @@ get_header();
 
     </div>
 
+    <div class="row mt-5">
+        <div class="col-12">
+            <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-3-scaled.jpg"
+                alt="">
+            <div class="brown-bg pt-4 pe-5 ps-5 pb-4">
+
+                <div class="service-subtitle white">Our Physiotherapeutic Services
+
+                </div>
+                <div class="mt-3">Our physiotherapists are chartered and Hong Kong registered with extensive
+                    experiences in musculoskeletal condition, sport injuries and work related conditions. Prevention
+                    and evidence-based practice are our desired objectives in all applied interventions. We educate
+                    our clients about general health and posture, especially how to prevent re-occurrence of same
+                    injury or complication and other conditions by specified techniques.
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
 
     <div class="service-subtitle">What Forms of Treatment Do We Offer?</div>
