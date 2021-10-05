d19c664424355af4acce00b4a6fefda667ae7901
kaiyu li <krisfk@gmail.com>
2021-10-06 05:55:00 +0800

    .

--- wp-content/themes/nextstepmedic/page-podiatry.php
+++ wp-content/themes/nextstepmedic/page-podiatry.php
@@ -350,7 +350,7 @@ get_header();
     </div>
 
 
-    <div class="row mt-5">
+    <div class="row mt-5 mobile-reverse">
 
         <div class="col-lg-6 col-md-12 col-sm-12 col-12">
             <div class="grey-bold">Orthotics / Insoles / Footwear
