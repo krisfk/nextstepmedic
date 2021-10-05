a62c373896577992e643f110f45f2b17c06b7bf8
kaiyu li <krisfk@gmail.com>
2021-10-06 05:35:13 +0800

    .

--- wp-content/themes/nextstepmedic/page-orthopaedics-traumatology.php
+++ wp-content/themes/nextstepmedic/page-orthopaedics-traumatology.php
@@ -79,7 +79,7 @@ get_header();
             </div>
         </div>
     </div>
-    <div class="row mt-5">
+    <div class="row mt-5 mobile-reverse">
 
         <div class="col-lg-6 col-md-12 col-sm-12 col-12">
             <div class="service-subtitle">Why Choose The Next Step?
