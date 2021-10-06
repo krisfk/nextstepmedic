bb0cc320720bdb1d427da4b3f924f1f6d797dfea
kaiyu li <krisfk@gmail.com>
2021-10-06 17:58:06 +0800

    .

--- wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
+++ wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
@@ -140,7 +140,7 @@ get_header();
 
 
                     </div>
-                    <div class="col-lg-6 col-md-12 col-sm-12 col-12"><img class="w-100"
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-12 mb-lg-0 mb-md-3 mb-sm-3 mb-3"><img class="w-100"
                             src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-13@2x-50-1.jpg" alt="">
                     </div>
 
