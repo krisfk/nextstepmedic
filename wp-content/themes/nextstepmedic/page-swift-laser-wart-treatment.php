f21b4dae66f1f6e4bc48ebd00bef479de1017e85
kaiyu li <krisfk@gmail.com>
2021-10-06 18:00:18 +0800

    .

--- wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
+++ wp-content/themes/nextstepmedic/page-swift-laser-wart-treatment.php
@@ -225,9 +225,9 @@ get_header();
                         Heat shock proteins within HPV infected cells are released, alerting the body to the presence of
                         the virus.
                     </div>
-                    <div class="col-lg-6 col-md-12 col-sm-12 col-12 position-relative">
-                        <iframe class="youtube-iframe mb-lg-0 mb-md-3 mb-sm-3 mb-3"
-                            src="https://www.youtube.com/embed/9y2-T_HRwto" title="YouTube video player" frameborder="0"
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-12 position-relative  mb-lg-0 mb-md-3 mb-sm-3 mb-3">
+                        <iframe class="youtube-iframe" src="https://www.youtube.com/embed/9y2-T_HRwto"
+                            title="YouTube video player" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
 
