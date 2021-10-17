bbc073255afb893b6b20f5ce1b514a8fe0a88a97
kaiyu li <krisfk@gmail.com>
2021-10-18 03:48:52 +0800

    .

--- wp-content/themes/nextstepmedic/page-orthopaedics-traumatology.php
+++ wp-content/themes/nextstepmedic/page-orthopaedics-traumatology.php
@@ -22,12 +22,14 @@ get_header();
 
             <div class="content-div service-title-div">
 
-                <h1 class="service-h1-title">Orthopaedics & Traumatology</h1>
+                <h1 class="service-h1-title">
+                    <!-- Orthopaedics & Traumatology -->
+                    <?php echo get_field('content_txt_0');?>
+                </h1>
             </div>
         </div>
     </div>
 
-    <!-- <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-1-scaled.jpg" alt=""> -->
 </div>
 
 
@@ -36,8 +38,9 @@ get_header();
 
     <div class="row mt-lg-5 mt-md-3 mt-sm-3 mt-3 ">
 
+        <?php echo get_field('content_txt_1');?>
 
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12"><img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-1@2x-50.jpg
+        <!-- <div class="col-lg-6 col-md-12 col-sm-12 col-12"><img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-1@2x-50.jpg
         " alt=""></div>
         <div class="col-lg-6 col-md-12 col-sm-12 col-12">
             <div class="service-subtitle mt-lg-0 mt-md-2 mt-sm-2  mt-2">What is Orthopaedics & Traumatology?</div>
@@ -49,19 +52,20 @@ get_header();
                 that you can start to feel good again.
             </div>
             <div class="mt-3">
-                <!-- <a href="<?php echo get_site_url();?>/contact-us-book-online" class="brown-round-btn">Book an
-                    Appointment</a> -->
+                
 
                 <a href="<?php echo get_site_url();?>/contact-us-book-online" class="book-an-appointment-a-btn">
                     Book an Appointment
                 </a>
 
             </div>
-        </div>
+        </div> -->
     </div>
 
     <div class="row mt-5">
-        <div class="col-12">
+        <?php echo get_field('content_txt_2');?>
+
+        <!-- <div class="col-12">
             <img class="w-100" src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-2@2x-50-1-scaled.jpg"
                 alt="">
             <div class="brown-bg pt-4 pe-5 ps-5 pb-4">
@@ -77,11 +81,13 @@ get_header();
                     and elbow, foot and ankle, hand and wrist, hip and spine.
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
     <div class="row mt-5 mobile-reverse">
 
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+        <?php echo get_field('content_txt_3');?>
+
+        <!-- <div class="col-lg-6 col-md-12 col-sm-12 col-12">
             <div class="service-subtitle mt-lg-0 mt-md-2 mt-sm-2  mt-2">Why Choose The Next Step?
             </div>
             <div class="mt-3">
@@ -97,13 +103,16 @@ get_header();
 
 
         <div class="col-lg-6 col-md-12 col-sm-12 col-12"><img class="w-100"
-                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-2.jpg" alt=""></div>
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-3@2x-50-2.jpg" alt=""></div> -->
     </div>
 
 
     <div class="row mt-5">
 
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12"><img class="w-100"
+        <?php echo get_field('content_txt_4');?>
+
+
+        <!-- <div class="col-lg-6 col-md-12 col-sm-12 col-12"><img class="w-100"
                 src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-4@2x-50-2.jpg" alt=""></div>
 
 
@@ -129,19 +138,21 @@ get_header();
                 <li>Shoulder Pain</li>
                 <li>Spine Pain</li>
                 <li>Sports Medicine</li>
-                <!-- <li>Paediatrics</li> -->
             </ul>
 
 
 
-        </div>
+        </div> -->
 
 
     </div>
 
     <div class="row mt-5 mobile-reverse">
 
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+        <?php echo get_field('content_txt_5');?>
+
+
+        <!-- <div class="col-lg-6 col-md-12 col-sm-12 col-12">
             <div class="service-subtitle mt-lg-0 mt-md-2 mt-sm-2  mt-2">Extensive Treatments Available
             </div>
 
@@ -173,7 +184,7 @@ get_header();
 
 
         <div class="col-lg-6 col-md-12 col-sm-12 col-12"><img class="w-100"
-                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-5@2x-50-1.jpg" alt=""></div>
+                src="https://nextstepmedic.com/wp-content/uploads/2021/09/Asset-5@2x-50-1.jpg" alt=""></div> -->
     </div>
 
 
