4f8bee2632bf3d497ea9fff7a539b7e47619532d
kaiyu li <krisfk@gmail.com>
2021-10-24 03:54:55 +0800

    .

--- wp-content/themes/nextstepmedic/page-testimonials.php
+++ wp-content/themes/nextstepmedic/page-testimonials.php
@@ -40,7 +40,62 @@ get_header();
 
 <div class="container testimonials-container mt-5 mb-5">
 
-    <div class="slides row">
+
+    <div class="row">
+
+        <div class="col-lg-4 col-md-12 col-sm-12 col-12 mb-4">
+
+            <p style="margin: 0px;"><a class="testimonials-foto-a" href="#"> <img class="w-100 testimonials-foto"
+                        src="https://nextstepmedic.com/wp-content/uploads/2021/10/person-1.jpg" alt=""> </a></p><a
+                class="testimonials-foto-a" href="#">
+                <div class="mt-4">“As a serious runner, enjoyment and the condition of my feet are inextricably linked
+                    and my regular appointments with Charlie have ensured that I can run in comfort at all
+                    times.”<br>“作為一名熱愛跑步的人，我雙腳的狀況和跑步的喜悅是密不可分的。我定期接受Charlie的治療，確保我可以舒適地跑步”</div>
+                <div class="person-name mt-3">Mr. Nicholas BROOKE, GBS, JP<br>蒲祿祺先生，金紫荊星章，太平紳士</div>
+            </a>
+            <p><a class="testimonials-foto-a" href="#"></a></p>
+
+
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12 col-12  mb-4">
+            <p style="margin: 0px;"><a href="#" class="testimonials-foto-a"> <img class="w-100 testimonials-foto"
+                        src="https://nextstepmedic.com/wp-content/uploads/2021/10/person-2.jpg" alt=""> </a></p><a
+                href="#" class="testimonials-foto-a">
+                <div class="mt-4">“Thank you for taking such good care of me and your many other patients. It is great
+                    to be able to run without any pain. Fizzing to get back into marathon”<br>“感謝您如此照顧我和您的許多其他患者 ,
+                    可以無憂無慮地跑步不再感到痛楚的感覺太好 ; 期待重返馬拉松賽跑<br>
+                    ” </div>
+                <div class="person-name mt-3">Mark Robinson Marathons runner<br>Mark Robinson 馬拉松賽跑選手
+                </div>
+            </a>
+            <p><a href="#" class="testimonials-foto-a"> </a></p>
+
+
+
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12 col-12  mb-4">
+
+            <p style="margin: 0px;"><a href="#" class="testimonials-foto-a"> <img class="w-100  testimonials-foto"
+                        src="https://nextstepmedic.com/wp-content/uploads/2021/10/person-3.jpg" alt=""> </a></p><a
+                href="#" class="testimonials-foto-a">
+                <div class="mt-4">“Charlie has been helping me with foot issues for a couple of years.&nbsp;&nbsp;I’m a
+                    runner, so experience a number of problems.&nbsp;He has been extremely helpful in suggesting
+                    options, exercises, and input on the type of shoes I should and should not wear.<p></p>
+                    <p>He takes the time to really understand the situation and ensure he is providing practical
+                        solutions.&nbsp;&nbsp;I recommend him to all my friends who are experiencing any type of foot
+                        pain.”</p>
+                </div>
+                <div class="person-name mt-3">Ms. Carole Lewis : Professional Certified Coach -International Coach
+                    Federation</div>
+            </a>
+            <p><a href="#" class="testimonials-foto-a"> </a></p>
+
+
+        </div>
+
+    </div>
+
+    <!-- <div class="slides row">
 
         <div class="slide col-lg-4 col-md-12 col-sm-12 col-12">
 
@@ -86,7 +141,7 @@ get_header();
 
         </div>
 
-    </div>
+    </div> -->
 
     <!-- 
     <div class="row mt-5">
