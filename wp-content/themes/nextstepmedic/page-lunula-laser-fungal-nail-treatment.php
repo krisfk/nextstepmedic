6241953d6118742532e06c0a04eaa6e10451d928
kaiyu li <krisfk@gmail.com>
2022-06-15 19:19:36 +0800

    .

--- wp-content/themes/nextstepmedic/page-lunula-laser-fungal-nail-treatment.php
+++ wp-content/themes/nextstepmedic/page-lunula-laser-fungal-nail-treatment.php
@@ -652,7 +652,7 @@ get_header();
 
     <div class="service-subtitle mt-5">Videos</div>
 
-    <div class="row">
+    <div class="row video-row-div">
 
         <div class="col-lg-6 col-md-12 col-sm-12 col-12">
 
